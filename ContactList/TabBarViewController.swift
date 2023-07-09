28b1b35f30e92c1bc117f02c7d6a8010ae77cb97
Luka Yurii <lukayurii.work@gmail.com>
2023-07-09 23:21:37 +0500

    DataStore

---
+++ ContactList/TabBarViewController.swift
@@ -0,0 +1,23 @@
+//
+//  TabBarViewController.swift
+//  ContactList
+//
+//  Created by Yuri Luka on 09/07/2023.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViewControllers()
+        }
+    
+    private func setupViewControllers() {
+        guard let contactListVC = viewControllers?.first as? ContactListViewController else { return }
+        guard let sectionVC = viewControllers?.last as? SectionTableViewController else { return }
+        
+        let persons = Person.getContactList()
+        
+    }
+}
---
+++ Model/Person.swift
@@ -0,0 +1,57 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Yuri Luka on 09/07/2023.
+//
+
+import Foundation
+
+struct Person {
+    
+    let name: String
+    let surname: String
+    let email: String
+    let phoneNumber: String
+    
+    var fullName: String {
+        "\(name) \(surname)"
+    }
+}
+
+extension Person {
+    static func getContactList() -> [Person] {
+        
+        var persons: [Person] = []
+        
+        let names = DataStore.shared.names.shuffled()
+        let surnames = DataStore.shared.surnames.shuffled()
+        let emails = DataStore.shared.emails.shuffled()
+        let phones = DataStore.shared.phones.shuffled()
+        
+        let iterationCount = min(
+            names.count,
+            surnames.count,
+            emails.count,
+            phones.count
+        )
+        
+        for index in 0..<iterationCount {
+            let person = Person(
+                name: names[index],
+                surname: surnames[index],
+                email: emails[index],
+                phoneNumber: phones[index]
+            )
+            
+            persons.append(person)
+        }
+        
+        return persons
+    }
+}
+
+enum ContactsL: String {
+    case phone = "phone"
+    case email = "tray"
+}
